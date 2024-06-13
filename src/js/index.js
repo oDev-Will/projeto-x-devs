efb0d1ce826209d736a340ce41014794c0fe7855
DevWil <wiljunior321@hotmail.com>
2024-06-13 09:02:09 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,38 @@
+const btnAvancar = document.getElementById('btn-avancar');
+const btnVoltar = document.getElementById('btn-voltar');
+let cartaoAtual = 0;
+const cartoes = document.querySelectorAll(".cartao");
+
+
+btnAvancar.addEventListener("click", function () {
+
+    if (cartaoAtual === cartoes.length - 1) return;
+    
+    
+
+    esconderCartaoSelecionado();
+    
+    cartaoAtual++;
+    mostrarCartaoSelecionado();
+
+});
+
+btnVoltar.addEventListener("click", function () {
+
+    if (cartaoAtual === 0) return;
+    
+    esconderCartaoSelecionado();
+
+    cartaoAtual--;
+    mostrarCartaoSelecionado();
+});
+
+function mostrarCartaoSelecionado() {
+    cartoes[cartaoAtual].classList.add("selecionado");
+}
+
+function esconderCartaoSelecionado() {
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+}
+
